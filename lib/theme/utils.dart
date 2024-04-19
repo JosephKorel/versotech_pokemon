d56648baa5550d86cde2ee31d51da9307593bbc5
José Carlos Rolim Etchepare <jcjosecarlos03@gmail.com>
2024-04-19 16:11:57 +0100

    add dio package and theme extension

---
+++ lib/theme/utils.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+extension ThemeHelper on BuildContext {
+  // Text theme
+  TextStyle get bodySmall => Theme.of(this).textTheme.bodySmall!;
+  TextStyle get bodyMedium => Theme.of(this).textTheme.bodyMedium!;
+  TextStyle get bodyLarge => Theme.of(this).textTheme.bodyLarge!;
+
+  TextStyle get titleSmall => Theme.of(this).textTheme.titleSmall!;
+  TextStyle get titleMedium => Theme.of(this).textTheme.titleMedium!;
+  TextStyle get titleLarge => Theme.of(this).textTheme.titleLarge!;
+
+  TextStyle get headlineSmall => Theme.of(this).textTheme.headlineSmall!;
+  TextStyle get headlineMedium => Theme.of(this).textTheme.headlineMedium!;
+  TextStyle get headlineLarge => Theme.of(this).textTheme.headlineLarge!;
+
+  // Colors
+  Color get primary => Theme.of(this).colorScheme.primary;
+  Color get onPrimary => Theme.of(this).colorScheme.onPrimary;
+  Color get primaryContainer => Theme.of(this).colorScheme.primaryContainer;
+  Color get onPrimaryContainer => Theme.of(this).colorScheme.onPrimaryContainer;
+  Color get secondary => Theme.of(this).colorScheme.secondary;
+  Color get onSecondary => Theme.of(this).colorScheme.onSecondary;
+  Color get tertiary => Theme.of(this).colorScheme.tertiary;
+  Color get onTertiary => Theme.of(this).colorScheme.onTertiary;
+  Color get background => Theme.of(this).colorScheme.background;
+  Color get onBackground => Theme.of(this).colorScheme.onBackground;
+  Color get onSurface => Theme.of(this).colorScheme.onSurface;
+  Color get error => Theme.of(this).colorScheme.error;
+  Color get onError => Theme.of(this).colorScheme.onError;
+}
+
+extension MediaQueryUtils on BuildContext {
+  double get deviceWidth => MediaQuery.of(this).size.width;
+  double get deviceHeight => MediaQuery.of(this).size.height;
+}
